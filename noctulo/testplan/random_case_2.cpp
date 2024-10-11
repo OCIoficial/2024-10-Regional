332ef9fb4a592ea40811c3a42dbdf49d778e2dd7
dabaez <mastil.daniel@gmail.com>
2024-10-10 23:16:17 -0300

    random case 2 works for test case 2

--- noctulo/testplan/random_case_2.cpp
+++ noctulo/testplan/random_case_2.cpp
@@ -7,26 +7,64 @@ int main(int argc, char *argv[]) {
   std::hash<std::string> hasher;
   std::mt19937 gen(hasher(argv[1]));
 
+  const int DAY = std::atoi(argv[2]);
+  assert(5 <= DAY && DAY <= 10);
+
   std::vector<int> fw(7, -1);
   std::vector<int> sw(5, -1);
-
-  std::uniform_int_distribution<int> dist(0, 4);
-  int day = dist(gen) + 5;
   std::uniform_int_distribution<int> random_day(0, 1);
-  fw[day % 7] = random_day(gen);
-  sw[day % 5] = fw[day % 7];
-  for (int i = 0; i < 7; i++) {
-    if (i < 5) {
-      if (fw[i] != -1) {
-        sw[i] = !fw[i];
-      } else if (sw[i] != -1) {
-        fw[i] = !sw[i];
-      } else {
-        fw[i] = random_day(gen);
-        sw[i] = !fw[i];
+  switch (DAY){
+    case 5:
+      for (int i=0;i<7;i++){
+        if (i < 5){
+          fw[i] = random_day(gen);
+          sw[i] = !fw[i];
+        }
+        if (i == 5) fw[i] = sw[0];
+        if (i == 6) fw[i] = random_day(gen);
       }
-    }
-    if (fw[i] == -1) fw[i] = random_day(gen);
+      break;
+    case 6:
+      for (int i=0;i<7;i++){
+        if (i < 5){
+          fw[i] = random_day(gen);
+          sw[i] = !fw[i];
+        }
+        if (i == 5) fw[i] = fw[0];
+        if (i == 6) fw[i] = sw[1];
+      }
+      break;
+    case 7:
+      for (int i=0;i<7;i++){
+        if (i == 2){
+          sw[i] = fw[0];
+          fw[i] = !sw[i];
+        } else if (i < 5){
+          fw[i] = random_day(gen);
+          sw[i] = !fw[i];
+        }
+        if (i == 5) fw[i] = fw[0];
+        if (i == 6) fw[i] = fw[1];
+      }
+      break;
+    case 8:
+      fw[0] = fw[5] = fw[2] = random_day(gen);
+      sw[0] = sw[2] = !fw[0];
+      fw[1] = fw[6] = sw[3] = random_day(gen);
+      sw[1] = fw[3] = !fw[1];
+      fw[4] = random_day(gen);
+      sw[4] = !fw[4];
+      break;
+    case 9:
+      fw[0] = fw[5] = fw[2] = sw[4] = random_day(gen);
+      sw[0] = sw[2] = fw[4] = !fw[0];
+      fw[1] = fw[6] = fw[3] = random_day(gen);
+      sw[1] = sw[3] = !fw[1];
+      break;
+    case 10:
+      fw[0] = fw[5] = fw[2] = fw[4] = sw[1] = sw[3] = random_day(gen);
+      sw[0] = sw[2] = sw[4] = fw[1] = fw[6] = fw[3] = !fw[0];
+      break;
   }
 
   for (int i = 0; i < 7; i++) {
