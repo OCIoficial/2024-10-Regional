2380329553f83622671f3a10949506f3e48e6966
Nico Lehmann <nico.lehmannm@gmail.com>
2024-10-17 10:03:23 -0700

    Noctulo: agregar validatores + arreglar formato casos manuales

--- noctulo/testplan/validators/st2.cpp
+++ noctulo/testplan/validators/st2.cpp
@@ -5,12 +5,4 @@ int main() {
   std::string ricardo = inf.readLine("[DN]{7}");
   std::string noctulo = inf.readLine("[DN]{5}");
   inf.readEof();
-
-  bool b = false;
-  for (int i = 0; i < 5; ++i) {
-    if (ricardo[i] == noctulo[i]) {
-      b = true;
-    }
-  }
-  ensuref(b, "no se encuentran en la primera semana");
 }
