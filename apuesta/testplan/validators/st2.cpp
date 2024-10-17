4e039a752120b3c2146e587998aacd680e694103
Nico Lehmann <nico.lehmannm@gmail.com>
2024-10-17 09:54:48 -0700

    Apuesta: agregar validadores y arreglar casos de prueba

--- apuesta/testplan/validators/st2.cpp
+++ apuesta/testplan/validators/st2.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   registerValidation();
-  int n = inf.readInt(1, 4, "n");
+  int n = inf.readInt(1, 100, "n");
   inf.readEoln();
   for (int i = 0; i < n; ++i) {
     if (i > 0) inf.readSpace();
