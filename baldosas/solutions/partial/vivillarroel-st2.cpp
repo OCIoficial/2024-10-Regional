50865876fab81ec3e52a27286e0d9fa4d061c4ed
Vicente Villarroel <ele38@pm.me>
2024-10-18 09:57:55 -0300

    Añadir solución st2 baldosas

---
+++ baldosas/solutions/partial/vivillarroel-st2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+// @ocimatic should-fail=[st1,st3,st4]
+int main(){
+  ll n, m;
+  cin >> n >> m;
+
+  cout << m - m/2 << ' ' << m/2 << '\n';
+}
